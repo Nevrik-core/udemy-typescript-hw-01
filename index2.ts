7d750955eeef8061de6ddf9fda7349d8970fa9bc
Nevrik-core <57769617+Nevrik-core@users.noreply.github.com>
2023-04-13 22:15:39 +0200

    hw-02 done

--- index2.ts
+++ index2.ts
@@ -9,30 +9,42 @@ const waterUserData = {
 	units: "m3",
 };
 
-const elRate = 0.45;
-const wRate = 2;
+const elRate: number = 0.45;
+const wRate: number = 2;
 
-const monthPayments = [0, 0]; // [electricity, water]
+const monthPayments: number[] = [0, 0]; // [electricity, water]
 
-const calculatePayments = (elData, wData, elRate, wRate) => {
-	if (elData.mode === "double" && elData.readings < 50) {
-		monthPayments[0] = elData.readings * elRate * 0.7;
+const calculatePayments = (
+	{ readings: elReadings, mode }: { readings: number, mode: string },
+	{ readings: wReadings }: { readings: number },
+	elRate: number,
+	wRate: number
+): void => {
+	if (mode === "double" && elReadings < 50) {
+		monthPayments[0] = elReadings * elRate * 0.7;
 	} else {
-		monthPayments[0] = elData.readings * elRate;
+		monthPayments[0] = elReadings * elRate;
 	}
 
-	monthPayments[1] = wData.readings * wRate;
+	monthPayments[1] = wReadings * wRate;
 };
 
 calculatePayments(electricityUserData, waterUserData, elRate, wRate);
 
-const sendInvoice = (monthPayments, electricityUserData, waterUserData) => {
-	const text = `    Hello!
+const sendInvoice = (
+	[el, water]: number[],
+	electricityUserData: { readings: number, units: string },
+	waterUserData: { readings: number, units: string }
+): string => {
+	const text: string = `    Hello!
     This month you used ${electricityUserData.readings} ${electricityUserData.units} of electricity
-    It will cost: ${monthPayments[0]}€
+    It will cost: ${el}€
     
     This month you used ${waterUserData.readings} ${waterUserData.units} of water
-    It will cost: ${monthPayments[1]}€`;
+    It will cost: ${water}€`;
 
 	return text;
 };
+
+const invoice = sendInvoice(monthPayments, electricityUserData, waterUserData);
+console.log(invoice);
