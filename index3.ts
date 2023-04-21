1b8c3e94ff96fd90f8920ba72d6cbee93d6c06f0
Nevrik-core <57769617+Nevrik-core@users.noreply.github.com>
2023-04-21 18:36:32 +0200

    hw-03 done

--- index3.ts
+++ index3.ts
@@ -1,38 +1,39 @@
 // структура данных склада с одеждой
 
-// ClothesWarehouse {
-// 	jackets: "empty" | number;
-// 	hats: "empty" | number;
-// 	socks: "empty" | number;
-// 	pants: "empty" | number;
-// }
+interface ClothesWarehouse {
+	jackets: "empty" | number;
+	hats: "empty" | number;
+	socks: "empty" | number;
+	pants: "empty" | number;
+}
 
 // структура данных склада с канцтоварами
 
-// StationeryWarehouse {
-// 	scissors: "empty" | number;
-// 	paper: "empty" | boolean;
-// }
+interface StationeryWarehouse {
+	scissors: "empty" | number;
+	paper: "empty" | boolean;
+}
 
 // структура данных склада с бытовой техникой
 
-// AppliancesWarehouse {
-// 	dishwashers: "empty" | number;
-// 	cookers: "empty" | number;
-// 	mixers: "empty" | number;
-// }
+interface AppliancesWarehouse {
+	dishwashers: "empty" | number;
+	cookers: "empty" | number;
+	mixers: "empty" | number;
+}
 
 // общая структура данных, наследует все данные из трех выше
 // + добавляет свои
 
-// TotalWarehouse {
-// 	deficit: boolean;
-// 	date: Date;
-// }
+interface TotalWarehouse extends ClothesWarehouse, StationeryWarehouse, AppliancesWarehouse {
+	deficit: boolean;
+	date: Date;
+}
 
 // главный объект со всеми данными, должен подходить под формат TotalWarehouse
 
-const totalData = {
+
+const totalData: TotalWarehouse = {
 	jackets: 5,
 	hats: "empty",
 	socks: "empty",
@@ -42,6 +43,8 @@ const totalData = {
 	dishwashers: 3,
 	cookers: "empty",
 	mixers: 14,
+	deficit: true,
+	date: new Date()
 };
 
 // Реализуйте функцию, которая принимает в себя главный объект totalData нужного формата
@@ -53,8 +56,16 @@ const totalData = {
 // "We need this items: hats, socks, cookers"
 // Товары через запятую, в конце её не должно быть. Пробел после двоеточия, в конце строки его нет.
 
-function printReport(data) {
-	return `We need this items: ${"..."}`;
+function printReport(data: TotalWarehouse): string {
+	let emptyItems = Object.entries(data)
+		.filter(([key, value]) => value === "empty")
+		.map(([key]) => key)
+		.join(", ");
+	
+	if (emptyItems.length > 0) {
+		return `We need this items: ${emptyItems}`;
+	} 
+	
 	// или
 	return "Everything fine";
 }
